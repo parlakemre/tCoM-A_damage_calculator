f5218e5b232c63a49e3aeffc526daade252a976e
parlakemre <86351395+parlakemre@users.noreply.github.com>
2022-10-06 09:38:49 +0300

    how to get a weapon?
    currently only has 1h weapon info

---
+++ obtainInfo.js
@@ -0,0 +1,315 @@
+var weaponWayToGet = {};
+
+weaponWayToGet[poker.name] = "common item";
+weaponWayToGet[skeleton_arm.name] = "ch1, at the beginning, lies nex to the skeleton";
+weaponWayToGet[heavy_branch.name] = "common item";
+weaponWayToGet[sickle.name] = "common item";
+weaponWayToGet[walking_stick.name] = "common item";
+weaponWayToGet[club.name] = "common item";
+weaponWayToGet[dagger.name] = "common item";
+weaponWayToGet[jils_pan.name] = "ch1, jil has it.";
+weaponWayToGet[knife_dex.name] = "common item";
+weaponWayToGet[wooden_sword.name] = "ch2, guard training if 1h mastery is the greatest";
+weaponWayToGet[timos_sword.name] = "prologue, let timo die, search his body on shore";
+weaponWayToGet[axe.name] = "from rupert after 'nobody stays behind' quest";
+weaponWayToGet[mace_of_condominium.name] = 
+  "ch1, near the waterfall between the volcano and silbach, next to a skeleton body";
+weaponWayToGet[rough_broadsword.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[wolf_knife.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[smiths_hammer.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[knife.name] = 
+  "ch1, on the body of the dead mearchant, near the charcoal burners camp";
+weaponWayToGet[wolf_knife_dex.name] = 
+  "ch1, thorsten sells it & in a barrel at guild mine";
+weaponWayToGet[small_scythe.name] = 
+  "ch1, in a barrel by the river bank between city gates";
+weaponWayToGet[rough_short_sword.name] = 
+  "ch1, in a cave with black goblings and shadowbeast near bermars farm";
+weaponWayToGet[spiked_club.name] = 
+  "ch1, sold by thorsten, carried by bandits, near a skeletal body between city gates";
+weaponWayToGet[meat_knife.name] = 
+  "ch1, blueprint is in the chest in thorsten's house";
+weaponWayToGet[militia_short_sword.name] = 
+  "ch1, blueprint is in the chest in thorsten's house";
+weaponWayToGet[spiked_cudgel.name] = 
+  "ch1, Guarded by goblins next to the skeleton on the way to the Amber Coast";
+weaponWayToGet[heavy_dagger.name] = 
+  "ch1, By the body of a woman on the beach, next to Bermar's farm";
+weaponWayToGet[sword.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[rough_hatchet.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[large_knife.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[short_sword.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[soldiers_sword.name] = 
+  "ch1, thorsten sells the blueprint";
+weaponWayToGet[beater.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[thorn.name] = 
+  "ch1, thorsten sells it";
+weaponWayToGet[wolfs_fang.name] = 
+  "ch1, thorsten sells it. ch2, In the body of a bandit in the cave behind Bermar's farm";
+weaponWayToGet[bandit_mace.name] = 
+  "ch2, sold by Ingor, Lennart, Odgar in the City. Held by bandits and loafers on Archolos";
+weaponWayToGet[sword_of_the_guard.name] = 
+  "ch2, odgar gaves the blueprint for the guard's weapon quest";
+weaponWayToGet[fine_short_sword.name] = 
+  "ch1, thorsten sells the blueprint";
+weaponWayToGet[skewer.name] = 
+  "ch1, thorsten sells the blueprint";
+weaponWayToGet[heavy_spiked_cudgel.name] = 
+  "ch2, sold by Lennart, Ingor, Odgar in the City. on the body in the sewers. ch3, sold by Walter. Used by bandits.";
+weaponWayToGet[arwids_sword.name] = 
+  "ch2, you get it from arwid if you beat him for the quest guard's weapon";
+weaponWayToGet[copper_dagger.name] = 
+  "ch2, sold by odgar. ch3, sold by ivan. bounty widow uses it";
+weaponWayToGet[rough_pernach.name] = 
+  "ch1, on a body near the stone circle near hunter darthe. ch2, in a chest in odgar's house, on the corpse in the sewers, sold by odgar, lenart, ingor in the city";
+weaponWayToGet[imminent_death.name] = 
+  "ch1, On a hill north of Rita's Vineyard. It is defended by two goblin warriors.";
+weaponWayToGet[amputator.name] = 
+  "ch2, sold by ingor, lenart, odgar. ch3, sold by ivan. used by pirates";
+weaponWayToGet[old_curved_sword.name] = 
+  "ch1,  In a chest in a shipwreck on the right side of the Amber Coast";
+weaponWayToGet[fine_sword_of_the_guard.name] = 
+  "ch2, odgar gives the blueprint for the guard's weapon quest if you are his apprentice";
+weaponWayToGet[war_hammer.name] = 
+  "ch2, sold by ingor, lenart, odgar. ch3, sold by ivan.";
+weaponWayToGet[engraved_sword.name] = 
+  "ch2, sold by ingor, lenart, odgar. ch3, sold by ivan. ch5, bounty havdan uses it";
+weaponWayToGet[general_sublis_baton.name] = 
+  "ch1, In the crypt in the forest with a stone circle next to Salzer.";
+weaponWayToGet[cutter.name] = 
+  "ch1, blueprint in a chest at the lighthouse near amber coast";
+weaponWayToGet[eksagnem.name] = 
+  "ch1, At the skeleton near the entrance to the mine north of the Valerio vineyard";
+weaponWayToGet[golden_dagger.name] = 
+  "ch1, blueprint sold by odgar";
+weaponWayToGet[poor_blade.name] = 
+  "ch2, sold by ingor, lenart, odgar. ch3, sold by ivan. ch4, bounty hyena uses it";
+weaponWayToGet[southerners_machete.name] = 
+  "ch2, blueprint in the chest at odgars home. southerners, pirates and bandits use it";
+weaponWayToGet[longsword.name] = 
+  "ch1, In the cave in the hole with drowned people next to the woodcutters' camp";
+weaponWayToGet[splitter.name] = 
+  "ch4, After defeating Culus for the third time. Used by bandits, Volker's mercenaries";
+weaponWayToGet[lousy_long_sword.name] = 
+  "ch1, On the rock ledge with black harpies below the troll to the right of the bridge to Vardhal";
+weaponWayToGet[missing_cutlass.name] = 
+  "ch1, In a damaged crate on the beach by the Monastery of Water Mages";
+weaponWayToGet[war_cudgel.name] = 
+  "ch1, On the islet with a tree near the fisherman's hut by the city walls.";
+weaponWayToGet[cutlass.name] = 
+  "ch3, sold by ivan. used by pirates.";
+weaponWayToGet[the_shiv.name] = 
+  "ch2, blueprint sold by odgar. we recieve it for free if we want to be his apprentice and have beginner blacksmithing";
+weaponWayToGet[light_combat_hammer.name] = 
+  "ch2, sold by ingor,odgar";
+weaponWayToGet[exquisite_sword_of_the_guard.name] = 
+  "ch2, odgar gives the blueprint for the guard's weapon quest if you are his apprentice and have lvl2 blacksmithing";
+weaponWayToGet[pirates_cutlass.name] = 
+  "ch1, at the ambear coast, near the sunken ship on shore";
+weaponWayToGet[spicker.name] = 
+  "ch2, blueprint sold by odgar";
+weaponWayToGet[chopper.name] = 
+  "ch2, blueprint sold by odgar";
+weaponWayToGet[large_axe.name] = 
+  "ch3, sold by ivan";
+weaponWayToGet[duty.name] = 
+  "ch1, on a corpse near black bear in the southern swamp";
+weaponWayToGet[southerners_sword_dex.name] = 
+  "ch2, blueprint in a chest in odgar's house";
+weaponWayToGet[fine_longsword.name] = 
+  "ch2, blueprint sold by odgar";
+weaponWayToGet[stone_breaker.name] = 
+  "ch1, in the tunnel between the city and guild mine.";
+weaponWayToGet[luise.name] = 
+  "ch5, salvi has it.";
+weaponWayToGet[decorated_sword.name] = 
+  "ch2, sold by ingor, odgar. ch3, sold by ivan. used by guards, mercenaries, king's men";
+weaponWayToGet[paladins_sword.name] = 
+  "ch2, used by runar";
+weaponWayToGet[squasher.name] = 
+  "ch3, sold by ivan";
+weaponWayToGet[double_bladed_axe.name] = 
+  "ch1, In the crypt, in the forest with a stone circle next to Salzer. on the way to the vardhal, on a corpse guarded by a dragon snapper";
+weaponWayToGet[great_axe.name] = 
+  "ch2, blueprint sold by odgar if apprentice";
+weaponWayToGet[bastard_sword.name] = 
+  "ch3, sold by ivan, in the otis's hut. ";
+weaponWayToGet[unfair_duel_sword.name] = 
+  "ch3, sold by ivan. used by bandits";
+weaponWayToGet[morgenstern.name] = 
+  "ch3, blueprint sold by miscellaneous merchant in haven.";
+weaponWayToGet[hand_cutter.name] = 
+  "ch3, sold by ivan. used by bandits";
+weaponWayToGet[knups_dagger.name] = 
+  "ch1, on an island north of archolos";
+weaponWayToGet[decorated_blade.name] = 
+  "ch2, blueprint sold by odgar";
+weaponWayToGet[smasher.name] = 
+  "ch3, sold by ivan.";
+weaponWayToGet[dueling_sword.name] = 
+  "ch2, blueprint obtained from Kurt for completing the quest Gold Rush if 1h% and dex are the highest. ch3, blueprint sold by Miscellaneous merchant in haven";
+weaponWayToGet[breaker.name] = 
+  "ch2, blueprint obtained from Kurt for completing the quest Gold Rush if 1h% and strre the highest. ch3, blueprint sold by Miscellaneous merchant in haven";
+weaponWayToGet[silver_plated_sword.name] = 
+  "ch4, sold by einar. used by grif,berto";
+weaponWayToGet[magic_ore_sword.name] = 
+  "ch3, reward for the quest 'in the silent fog' (?) if 1h% > 2h%";
+weaponWayToGet[alenas_sword_str.name] = 
+  "ch2, reward for winning the tournament in the slums if str>dex";
+weaponWayToGet[boughs_destroyer.name] = 
+  "ch3, javad uses it. volker's men use it";
+weaponWayToGet[rapier.name] = 
+  "ch3, get it from becket if we let him escape in the quest old friends. sold by ivan";
+weaponWayToGet[bloody_claw.name] = 
+  "ch3, sold by lenart if mercenary. ch4, sold by einar. used by volker's men, pirates";
+weaponWayToGet[gigus.name] = 
+  "ch1, in a cave near the watchtawer behind bermar's farm.";
+weaponWayToGet[ruby_blade.name] = 
+  "ch1, in a chest in the cave with spooky blacksmith ghost in the misty marshes";
+weaponWayToGet[elisium.name] = 
+  "ch1, at the shipwreck near the monastery.";
+weaponWayToGet[the_fear_of_logs.name] = 
+  "ch3, blueprint in the chest under the lighthouse. treasure map can be found in cortez's chest or on the body of the dead mercenary in the royal mine.";
+weaponWayToGet[flash.name] = 
+  "ch1, in a cave from north of silbach, guarded by a black bear";
+weaponWayToGet[blessed_magic_blade.name] = 
+  "ch4, saradoc has it (lvl 50 required to spawn)";
+weaponWayToGet[song_of_the_fallen.name] = 
+  "ch1, in a crypt with fallen paladins near sailor's retreat";
+weaponWayToGet[long_magic_sword.name] = 
+  "ch4, sold by einar. arwid will have it if you defeated him before in a sword duel.";
+weaponWayToGet[light_master_sword.name] = 
+  "ch3, ezekiel will sell it for cheap if you ask him about the contract. in the last treasure of the quest buried treasure.";
+weaponWayToGet[war_mace.name] = 
+  "ch4, From Ulrich after completing the quest Journey to the interior of the mine, when the skill of fighting with 1H weapons and strength are the highest.";
+weaponWayToGet[ivys_dagger.name] = 
+  "end of the gayme";
+weaponWayToGet[kushis_sword.name] = 
+  "ch1, On the misty marshes, on the rock by the swamp troll";
+weaponWayToGet[ravens_beak.name] = 
+  "ch1, In the cave with the shadowbeasts in the valley in front of the guild mine";
+weaponWayToGet[firm_sword_str.name] = 
+  "ch4, as a reward from a Blacksmith near Odgar for giving him 2 bars if str> dex, this is a random event";
+weaponWayToGet[usurers_blade.name] = 
+  "end of the game";
+weaponWayToGet[slasher.name] = 
+  "ch4, In the box behind Foral's hut inside the Wolf's den";
+weaponWayToGet[poor_magic_blade.name] = 
+  "ch4, blueprint sold by einar";
+weaponWayToGet[executioners_axe.name] = 
+  "ch4, sold by einar";
+weaponWayToGet[alenas_sword.name] = 
+  "ch2, reward for winning the tournament in the slums if dex>str";
+weaponWayToGet[manfreds_shiv.name] = 
+  "ch1, in a chest in front of the forgotten temple in the southern swamps";
+weaponWayToGet[long_saber.name] = 
+  "ch3, blueprint in cortez's chest, sold by misc. trader in haven";
+weaponWayToGet[balanced_spiked_club.name] = 
+  "ch4, sold by einar";
+weaponWayToGet[stroker.name] = 
+  "ch5, blueprint given by odgar if apprentice and 1h dex are max.";
+weaponWayToGet[inquisitor.name] = 
+  "ch4, blueprint sold by einar";
+weaponWayToGet[dueling_saber.name] = 
+  "ch2, in a chest in venzel's";
+weaponWayToGet[el_bastardo.name] = 
+  "ch5, sold by einar, ingor if lvl2 mercenary, lenart if lvl2 swrodsmen";
+weaponWayToGet[firm_sword.name] = 
+  "ch4, as a reward from a Blacksmith near Odgar for giving him 2 bars if dex> str, this is a random event";
+weaponWayToGet[ramseys_ancestral_sword.name] = 
+  "ch4, For completing the quest The Beast of Archolos, if 1h% and str are max.";
+weaponWayToGet[the_fear_of_sailors.name] = 
+  "ch3, blueprint as a  Reward for the Legendary Recipe quest, if 1h% > 2h% and we tell Odgar the truth. ch5, blueprint sold by einar.";
+weaponWayToGet[cleaver.name] = 
+  "ch5, blueprint sold by einar";
+weaponWayToGet[rune_sword.name] = 
+  "ch5, sold by einar, sold by ingor if mercenary lvl2, sold by lenart if swordsmen lvl2";
+weaponWayToGet[abu_dubas_scimitar.name] = 
+  "ch2, on the alter in the forgotten temple";
+weaponWayToGet[corsair_of_the_oceans.name] = 
+  "ch4, einar sells it. ch5, in a chest in vardhal";
+weaponWayToGet[pride_of_arholos.name] = 
+  "ch5, sold by einar. sold by ingor if lvl2 mercenary. sold by lenart if lvl2 swordsmen";
+weaponWayToGet[master_sword.name] = 
+  "ch3, blueprint in the pirate's chest under the lighthouse";
+weaponWayToGet[ramseys_rod_rapier.name] = 
+  "ch4, reward for completing the quest beast of archolos if dex>str and 1h% > 2h%";
+weaponWayToGet[balanced_blade.name] = 
+  "ch5, sold by einar. sold by ingor if lvl2 mercenary. sold by lenart if lvl2 swordsmen";
+weaponWayToGet[magic_combat_blade.name] = 
+  "ch2, blueprint in a chest in firemages' house upper floor.";
+weaponWayToGet[sword_of_archolos_defender.name] = 
+  "ch5, from lorenzo or roderich after completing the quest crownless king if 1h% > 2h%";
+weaponWayToGet[the_blade_of_justice.name] = 
+  "ch5, sold by einar. sold by ingor if lvl2 mercenary. sold by lenart if lvl2 swordsmen";
+weaponWayToGet[blade_with_a_blue_stone.name] = 
+  "ch6, sold by thorsten after leaving vardhal";
+weaponWayToGet[blade_with_a_blue_stone.name] = 
+  "ch5, sold by einar";
+weaponWayToGet[silver_slayer.name] = 
+  "ch5, sold by einar. sold by ingor if lvl2 mercenary. sold by lenart if lvl2 swordsmen";
+weaponWayToGet[fury.name] = 
+  "ch6, sold by thorsten after leaving vardhal";
+weaponWayToGet[stuffer.name] = 
+  "ch5, in the package given by pablo before going to vardhal if dex>str and 1h% > 2h%";
+weaponWayToGet[wrath_of_innos.name] = 
+  "ch5, in the armory in the vardhal";
+weaponWayToGet[fine_bastard_sword.name] = 
+  "ch4, blueprint sold by einar";
+weaponWayToGet[captains_sabre.name] = 
+  "ch5, sold by einar. sold by ingor if lvl2 mercenary. sold by lenart if lvl2 swordsmen";
+weaponWayToGet[pure_ore_magic_blade.name] = 
+  "ch5, blueprint in the library in the vardhal";
+weaponWayToGet[cortezs_sword.name] = 
+  "ch5, after killing cortez.";
+weaponWayToGet[corsair_pride.name] = 
+  "ch6, sold by thorsten after leaving vardhal";
+weaponWayToGet[the_peacemaker.name] = 
+  "ch5, in vardhal, unusable";
+weaponWayToGet[shining_sword.name] = 
+  "ch5, on a corpse in the room with the fireplace in vardhal";
+weaponWayToGet[sailor_of_storms.name] = 
+  "ch5, blueprint in the library in the vardhal";
+
+
+// 2H WEAPONS
+weaponWayToGet[wooden_sword_2h.name] = "ch2, guard training if 2h mastery is the greatest";
+
+
+
+
+
+
+
+
+
+
+
+function howToGet(weapon) {
+  
+  if (pWayToGet) {
+    pWayToGet.remove();
+    pWayToGet = undefined;
+  }
+  pWayToGet=createP(weaponWayToGet[weapon.name]).position(pSuggestion.position().x,
+                                                         pSuggestion.position().y+15);
+}
+
+function howToGet_dropdown(weapon) {
+  if (pHowToGet) {
+    pHowToGet.remove();
+    pHowToGet = undefined;
+  }
+  
+  pHowToGet=createP(weaponWayToGet[weapon.name]).position(pSwords.position().x,
+                                                         pSwords.position().y+5);
+  
+}
