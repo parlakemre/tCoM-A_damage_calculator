4df4886aa5d1703fa7ff4fb322619e8089b33618
parlakemre <86351395+parlakemre@users.noreply.github.com>
2022-10-06 09:37:25 +0300

    a Monster class
    and all the monster infos

---
+++ monsterInfo.js
@@ -0,0 +1,398 @@
+class Monster {
+  constructor(name,life,blunt,edge,point,fire,wind,magic) {
+    this.name = name;
+    this.life = life;
+    this.blunt = blunt;
+    this.edge = edge;
+    this.point = point;
+    this.fire = fire;
+    this.wind = wind;
+    this.magic = magic;
+  }
+}
+
+let overgrown_snapper = new Monster("overgrown snapper",
+                                    500,130,130,125,35,55,40);
+
+let undead_lord = new Monster("undead lord",
+                                    7000,120,135,150,666,500,0);
+
+let undead_battle_mage = new Monster("undead battle mage",
+                                    3000,80,100,110,666,40,0);
+
+let minecrawler_queen = new Monster("minecrawler queen",
+                                    10000,90,60,70,0,Infinity,0);
+
+let armored_razor = new Monster("armored razor",
+                                    1500,140,150,145,0,120,0);
+
+let slickhand = new Monster("slickhand",
+                                    500,20,10,15,0,0,0); 
+
+let wetty = new Monster("wetty",
+                                    650,55,55,55,0,0,0);
+	
+let axeman = new Monster("axeman",
+                                    600,15,15,15,5,0,0);	
+
+let toll_collector = new Monster("toll collector",
+                                    780,45,45,40,20,0,10);
+
+let sewerman = new Monster("sewerman",
+                                    500,55,40,50,10,0,0);
+
+let rebellious_novice = new Monster("rebellious novice",
+                                    850,25,25,25,0,0,10);
+	
+let frost = new Monster("frost",
+                                    1000,100,100,100,35,0,45);	
+
+let the_hyena = new Monster("the hyena",
+                                    1000,90,90,90,90,0,90);
+
+let widow = new Monster("widow",
+                                    1000,15,15,15,10,0,0);
+
+let puma_human = new Monster("puma human",
+                                    1000,70,75,85,100,0,20);
+
+let puma_monster = new Monster("puma monster",
+                                    1000,70,30,30,10,50,10);
+	
+let the_prophet = new Monster("the prophet",
+                                    1000,20,10,15,0,0,0);
+
+let prophets_faithful = new Monster("prophets faithful",
+                                    500,35,50,35,5,0,5);
+
+let havdan = new Monster("havdan",
+                                    1500,95,120,95,35,0,15);
+
+let rabid_boar = new Monster("rabid boar",
+                                    550,50,60,45,10,20,15);
+
+let overgrown_scavenger = new Monster("overgrown scavenger",
+                                    600,30,30,15,200,30,10);
+
+let fatty = new Monster("fatty",
+                                    700,35,35,35,10,35,10);
+
+let hardy = new Monster("hardy",
+                                    1000,85,125,130,30,65,35);
+	
+let the_bloodsucker = new Monster("the bloodsucker",
+                                    850,115,115,80,35,15,40);	
+
+let ice_wolf = new Monster("ice wolf",
+                                    720,80,80,45,15,25,30);
+	
+let albino_bear = new Monster("albino bear",
+                                    1250,190,170,200,160,310,110);	
+
+let sleeping_shadowbeast = new Monster("sleeping shadowbeast",
+                                    1000,150,150,150,150,150,120);
+
+let sick_rat = new Monster("sick rat",
+                                    800,80,80,125,45,60,70);
+
+let crystal_golem = new Monster("crystal golem",
+                                    1500,130,150,220,120,220,-1);
+
+let mountain_troll = new Monster("mountain troll",
+                                    1200,200,200,270,160,220,100);
+
+let dragon = new Monster("dragon",
+                                    1200,170,170,190,-1,75,125);
+
+let disturbing_beast = new Monster("disturbing beast",
+                                    200,75,70,75,15,25,20);
+
+let goblin = new Monster("goblin",
+                                   20,20,20,25,0,0,0);
+
+let giant_rat = new Monster("giant rat",
+                                   30,15,15,10,15,15,0);
+
+let skeleton = new Monster("skeleton",
+                                   35,15,30,50,30,0,30);
+	
+let scavenger = new Monster("scavenger",
+                                   40,20,20,5,20,20,0);	
+
+let bloodfly = new Monster("bloodfly",
+                                   40,20,20,20,20,20,0);	
+
+let black_goblin = new Monster("black goblin",
+                                   40,40,40,55,0,0,0);
+
+let minecrawler_larva = new Monster("minecrawler larva",
+                                   50,20,20,70,20,20,20);
+
+let sewer_drone = new Monster("sewer drone",
+                                   60,25,25,5,0,40,10);
+	
+let molerat = new Monster("molerat",
+                                   60,25,25,25,0,25,0);	
+
+let wolf = new Monster("wolf",
+                                   60,35,30,5,0,0,0);	
+
+let goblin_warrior = new Monster("goblin warrior",
+                                   75,75,75,85,15,20,15);	
+
+let field_raider = new Monster("field raider",
+                                   80,40,40,40,0,0,0);	
+
+let river_bloodfly = new Monster("river bloodfly",
+                                   90,35,35,35,10,30,5);
+	
+let mudturtle = new Monster("mudturtle",
+                                   100,20,45,50,0,25,5);	
+
+let boar = new Monster("boar",
+                                   100,40,50,35,0,10,5);
+
+let swamp_drone = new Monster("swamp drone",
+                                   100,60,30,10,15,100,15);
+	
+let goblin_skeleton = new Monster("goblin skeleton",
+                                   120,30,60,100,40,0,50);	
+
+let lizard = new Monster("lizard",
+                                   120,50,50,60,60,40,10);
+
+let lurker = new Monster("lurker",
+                                   120,60,60,60,0,10,5);
+
+let black_wolf = new Monster("black wolf",
+                                   120,65,65,20,0,10,15);
+	
+let forest_scavenger = new Monster("forest scavenger",
+                                   145,65,65,30,0,10,10);	
+
+let snapper = new Monster("snapper",
+                                   150,70,70,75,20,15,15);
+
+let swamp_zombie = new Monster("swamp zombie",
+                                   150,75,75,75,75,75,0);
+
+let skeleton_warrior = new Monster("skeleton warrior",
+                                   150,90,120,170,30,10,50);
+	
+let harpy = new Monster("harpy",
+                                   180,90,90,40,90,0,25);	
+
+let mantis = new Monster("mantis",
+                                   190,65,50,60,0,15,15);	
+
+let swamp_bloodfly = new Monster("swamp bloodfly",
+                                   190,75,75,80,15,40,20);
+
+let swamp_rat = new Monster("swamp rat",200,60,60,60,10,20,10);
+
+let black_rat = new Monster("black rat",200,60,60,75,25,40,15);
+
+let exhausted_scavenger = new Monster("exhausted scavenger",
+                                      200,70,70,25,200,10,30);
+
+let minecrawler = new Monster("minecrawler",
+                                      200,75,60,70,10,15,20);
+
+let forest_raider = new Monster("forest raider",
+                                      200,75,70,75,15,25,20);
+	
+let sea_lurker = new Monster("sea lurker",
+                                      200,75,75,75,50,15,10);	
+
+let minecrawler_drone = new Monster("minecrawler drone",
+                                      200,80,55,90,100,20,50);
+
+let swamp_lizard = new Monster("swamp lizard",
+                                      220,100,100,110,60,50,30);
+
+let orc_dog = new Monster("orc dog",
+                                      220,110,110,95,25,30,45);
+
+let old_lurker = new Monster("old lurker",
+                                      240,80,80,60,0,60,0);
+
+let razor = new Monster("razor",
+                                      240,125,125,120,30,40,35);
+
+let alligator = new Monster("alligator",
+                                      250,70,70,80,50,35,20);
+	
+let giant_bat = new Monster("giant bat",
+                                      250,100,100,65,20,0,25);
+	
+let warg = new Monster("warg",
+                                      250,110,110,100,40,40,55);	
+
+let stone_golem = new Monster("stone golem",
+                                      250,110,140,200,100,200,60);
+
+let crab = new Monster("crab",
+                                      270,70,110,115,15,50,25);
+
+let frogman = new Monster("frogman",
+                                      275,70,65,75,50,20,35);
+
+let skeleton_wolf_son = new Monster("skeleton wolf son",
+                                      280,95,130,140,0,30,70);
+
+let swamp_golem = new Monster("swamp_golem",
+                                      280,100,130,180,0,200,75);
+	
+let mantis_warrior = new Monster("mantis warrior",
+                                      280,120,100,135,40,35,55);
+
+let cursed_goblin_skeleton = new Monster("cursed goblin skeleton",
+                                      300,80,120,145,60,50,80);
+
+let bear = new Monster("bear",
+                                      300,100,90,110,45,125,35);
+
+let skeleton_mage = new Monster("skeleton mage",
+                                      300,120,135,150,0,125,95);
+
+let cursed_snapper = new Monster("cursed snapper",
+                                      300,140,140,150,70,60,60);
+
+let orc_scout = new Monster("orc scout",
+                                      320,70,70,70,30,35,30);
+	
+let black_harpy = new Monster("black harpy",
+                                      320,115,115,60,110,40,50);	
+
+let worm = new Monster("worm",
+                                      320,120,100,140,100,80,50);
+
+let minecrawler_warrior = new Monster("minecrawler warrior",
+                                      320,140,130,150,50,70,45);
+
+let slimy_lizard = new Monster("slimy lizard",
+                                      350,140,140,160,200,140,80);	
+
+let black_forest_raider = new Monster("black forest raider",
+                                      350,160,150,175,100,100,75);	
+
+let fire_lizard = new Monster("fire lizard",
+                                      360,150,150,180,-1,55,180);
+	
+let swampshark = new Monster("swampshark",
+                                      400,110,90,120,100,100,35);	
+	
+let cursed_wolf = new Monster("cursed wolf",
+                                      400,140,140,130,45,150,90);	
+
+let dragon_snapper = new Monster("dragon snapper",
+                                      400,190,190,180,120,80,100);
+	
+let ice_golem = new Monster("ice golem",
+                                      450,110,160,210,0,160,160);
+
+let spiky_alligator = new Monster("spiky alligator",
+                                      450,120,120,140,100,100,70);
+
+let skeleton_wolf_son_in_vardhal = new Monster(
+  "skeleton wolf son in vardhal",450,120,140,160,20,70,100);
+	
+let shadowbeast_skeleton = new Monster("shadowbeast skeleton",
+                                      450,130,170,210,200,50,150);	
+
+let orc_warrior = new Monster("orc warrior",
+                                      450,140,140,150,70,150,80);	
+
+let orc_crossbowman = new Monster("orc crossbowman",
+                                      480,105,105,105,70,105,60);
+
+let orc_shaman = new Monster("orc shaman",
+                                      490,90,90,120,120,80,70);
+
+let zombie = new Monster("zombie",
+                                      500,110,100,140,10,50,60);
+
+let zombie_wolf_son = new Monster("zombie wolf son",
+                                      500,150,140,170,0,50,75);
+
+let swamp_troll = new Monster("swamp troll",
+                                      500,150,150,220,100,180,120);
+
+let cursed_golem = new Monster("cursed golem",
+                                      500,170,200,220,150,220,140);
+
+let cemetery_golem = new Monster("cemetery golem",
+                                      500,180,220,250,200,250,150);
+
+let fallen_paladin = new Monster("fallen paladin",
+                                      500,200,230,230,100,60,125);
+	
+let shadowbeast = new Monster("shadowbeast",
+                                      550,140,140,140,140,140,110);
+	
+let seashark = new Monster("seashark",
+                                      550,170,160,190,200,200,90);	
+
+let orc_elite = new Monster("orc elite",
+                                      575,180,180,200,100,180,150);
+
+let black_bear = new Monster("black bear",
+                                      600,180,160,190,150,300,100);
+
+let tomb_guardian = new Monster("tomb guardian",
+                                      700,140,160,160,20,50,125);
+	
+let troll = new Monster("troll",700,180,180,250,140,200,150);
+
+let archdemon = new Monster("archdemon",700,220,220,230,666,666,200);
+
+let royal_minecrawler = new Monster("royal minecrawler",
+                                    750,160,150,170,70,85,75);
+
+let orc_warlord = new Monster("orc-warlord",
+                                    1000,170,170,190,100,170,130);
+
+let cursed_troll = new Monster("cursed troll",
+                                    1000,200,200,270,150,300,180);	
+
+let gargoyle = new Monster("gargoyle",
+                                    1700,120,140,130,500,Infinity,90);	
+
+monsterList = [overgrown_snapper,undead_lord,undead_battle_mage,
+                  minecrawler_queen,armored_razor,slickhand,
+                  wetty,axeman,toll_collector,sewerman,rebellious_novice,
+                  frost,the_hyena,widow,puma_human,puma_monster,
+                  the_prophet,prophets_faithful,havdan,rabid_boar,
+                  overgrown_scavenger,fatty,hardy,the_bloodsucker,
+                  ice_wolf,albino_bear,sleeping_shadowbeast,
+                  sick_rat,crystal_golem,mountain_troll,dragon,
+                  disturbing_beast,goblin,giant_rat,skeleton,scavenger,
+                  bloodfly,black_goblin,minecrawler_larva,sewer_drone,
+                  molerat,wolf,goblin_warrior,field_raider,
+                  river_bloodfly,mudturtle,boar,swamp_drone,
+                  goblin_skeleton,lizard,lurker,black_wolf,
+                  forest_scavenger,snapper,swamp_zombie,skeleton_warrior,
+                  harpy,mantis,swamp_bloodfly,swamp_rat,black_rat,
+                  exhausted_scavenger,minecrawler,forest_raider,
+                  sea_lurker,minecrawler_drone,swamp_lizard,
+                  orc_dog,old_lurker,razor,alligator,giant_bat,warg,
+                  stone_golem,crab,frogman,skeleton_wolf_son,
+                  swamp_golem,mantis_warrior,cursed_goblin_skeleton,
+                  bear,skeleton_mage,cursed_snapper,orc_scout,
+                  black_harpy,worm,minecrawler_warrior,slimy_lizard,
+                  black_forest_raider,fire_lizard,swampshark,cursed_wolf,
+                  dragon_snapper,ice_golem,spiky_alligator,
+                  skeleton_wolf_son_in_vardhal,shadowbeast_skeleton,
+                  orc_warrior,orc_crossbowman,orc_shaman,zombie,
+                  zombie_wolf_son,swamp_troll,cursed_golem,
+                  cemetery_golem,fallen_paladin,shadowbeast,seashark,
+                  orc_elite,black_bear,tomb_guardian,troll,archdemon,
+                  royal_minecrawler,orc_warlord,cursed_troll,gargoyle];
+
+monsterBosses = monsterList.slice(0,5).sort(sortByName);
+monsterBounty = monsterList.slice(5,32).sort(sortByName);
+monsterWeak = monsterList.slice(32,59).sort(sortByName);
+monsterMedium = monsterList.slice(59,90).sort(sortByName);
+monsterStrong = monsterList.slice(90).sort(sortByName);
+
+monsterList.sort(sortByName);
